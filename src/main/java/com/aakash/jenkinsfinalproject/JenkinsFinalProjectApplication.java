8187715e3900016c30c3d2c3560c62be68f097a2
AakashNakarmi <aakasnakarmee@gmail.com>
2024-01-29 23:11:48 +0530

    DeploymentService.yaml Updated

--- src/main/java/com/aakash/jenkinsfinalproject/JenkinsFinalProjectApplication.java
+++ src/main/java/com/aakash/jenkinsfinalproject/JenkinsFinalProjectApplication.java
@@ -2,9 +2,17 @@ package com.aakash.jenkinsfinalproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class JenkinsFinalProjectApplication {
+
+	@GetMapping
+	public String message(){
+		return "Welcome to Spring Microservices";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(JenkinsFinalProjectApplication.class, args);
